53e2c4557bee258cdd3c5027d24138a337bb31bb
jessor <daniel@enthusiasm.cc>
2014-11-28 16:55:30 +0100

    Adds deposit calculations to public

--- public/class-woocommerce-pfand-public.php
+++ public/class-woocommerce-pfand-public.php
@@ -50,7 +50,13 @@ class Woocommerce_Pfand_Public {
 	public function __construct( $woocommerce_pfand, $version ) {
 
 		$this->woocommerce_pfand = $woocommerce_pfand;
-		$this->version = $version;
+        $this->version = $version;
+
+        add_filter( 'woocommerce_get_price_suffix' , array( $this, 'add_deposit_value_to_price_suffix' ), 10, 2 );
+        add_filter( 'woocommerce_cart_item_price', array( $this, 'add_deposit_value_to_cart_item_price' ), 10, 3 );
+        add_filter( 'woocommerce_cart_item_subtotal', array( $this, 'add_deposit_value_to_cart_item_subtotal' ), 10, 3 );
+
+        add_action( 'woocommerce_cart_calculate_fees', array( $this, 'add_deposit_value_to_totals' ) );
 
 	}
 
@@ -96,8 +102,103 @@ class Woocommerce_Pfand_Public {
 		 * class.
 		 */
 
-		wp_enqueue_script( $this->woocommerce_pfand, plugin_dir_url( __FILE__ ) . 'js/woocommerce-pfand-public.js', array( 'jquery' ), $this->version, false );
+        wp_enqueue_script( $this->woocommerce_pfand, plugin_dir_url( __FILE__ ) . 'js/woocommerce-pfand-public.js', array( 'jquery' ), $this->version, false );
 
-	}
+    }
+
+    /**
+     * Adds deposit value to price displays
+     * e.g. archive, content, single, cart
+     *
+     * @since   1.0.0
+     */
+    function add_deposit_value_to_price_suffix( $price_display_suffix, $product ) {
+
+        $dep_total = $this->get_deposit( $product->id );
+
+        if( ! empty( $dep_total ) ) {
+
+            // Remove </small>
+            $price_display_suffix = preg_replace( '/<\/small>/', '', $price_display_suffix, 1 );
+
+            $price_display_suffix .= '<br />';
+            $price_display_suffix .= sprintf( __( 'plus deposit of %s', $this->woocommerce_pfand ), wc_price( $dep_total ) );
+
+            $price_display_suffix .= '</small>';
+
+        }
+
+        return $price_display_suffix;
+    }
+
+    /**
+     * Adds deposit value to item price displays in the cart
+     *
+     * @since   1.0.0
+     */
+    function add_deposit_value_to_cart_item_price( $product_price, $values, $cart_item_key ) {
+ 
+        $dep_total = $this->get_deposit( $values['product_id'] );
+
+        if( ! empty( $dep_total ) ) {
+            return $product_price . '<br /><small class="deposit_label">' .  sprintf( __( 'plus deposit of %s', $this->woocommerce_pfand ), wc_price( $dep_total ) ) . '</small>';
+        }
+ 
+        return $product_price;
+    }
+
+    /**
+     * Adds deposit value to subtotal display in cart
+     *
+     * @since   1.0.0
+     */
+    function add_deposit_value_to_cart_item_subtotal( $product_price, $values, $cart_item_key ) {
+ 
+        $dep_total = $this->get_deposit( $values['product_id'], $values['quantity'] );
+
+        if( ! empty( $dep_total ) ) {
+            return $product_price . '<br /><small class="deposit_label">' .  sprintf( __( 'plus deposit of %s', $this->woocommerce_pfand ), wc_price( $dep_total ) ) . '</small>';
+        }
+ 
+        return $product_price;
+    }
+
+    /**
+     * Adds deposit value to totals
+     */
+    function add_deposit_value_to_totals() {
+        global $woocommerce;
+
+        $dep_total = 0;
+        foreach ( WC()->cart->get_cart() as $cart_item ) {
+            $dep_total = $dep_total + $this->get_deposit( $cart_item['product_id'], $cart_item['quantity'] );
+        }
+
+        $woocommerce->cart->add_fee( __( 'Deposit Total', $this->woocommerce_pfand ), $dep_total );
+    }
+
+    /**
+     * Function to calculate deposits
+     *
+     * @since   1.0.0
+     */
+    function get_deposit( $id, $quantity = 1 ) {
+
+        $terms = get_the_terms( $id, 'product_deptype' );
+
+        if( $terms && ! is_wp_error( $terms ) ) {
+            $dep_total = 0;
+            foreach( $terms as $term ) {
+                $dep_total = $dep_total + $term->name;
+            }
+        }  
+
+        if( $dep_total > 0 ) {
+            $dep_total = $dep_total * $quantity;
+            return $dep_total;
+        }
+
+        return false;
+    }
 
 }
